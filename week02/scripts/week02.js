af59cb535934df8a6401108176113c9180854935
TheSergei <squintanilla@byupathway.edu>
2025-03-11 21:50:12 -0500

    temple album

---
+++ week02/scripts/week02.js
@@ -0,0 +1,17 @@
+document.addEventListener('click', function(){
+    if (input.value.trim() !== '');
+    const theInput = document.querySelector(input);
+    const theButton = document.querySelector(button);
+    const theList = document.querySelector(' ');
+
+    const list = document.createElement('li');
+    const deleteButton = document.createElement('button');
+
+    li.textContent = input.value;
+    deleteButton.textContent = '❌'
+
+    li.append(deleteButton);
+
+    list.append(li);
+
+    });
